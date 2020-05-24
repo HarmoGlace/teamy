b02708e7afeb1713bc2cce07a1661eec1e658ec4
HarmoGlace <harmoglacehelp@gmail.com>
2020-05-24 12:54:39 +0200

    Exported handlers

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ module.exports = {
     TeamsManager: require('./structures/team/TeamsManager'),
     Team: require('./structures/team/Team'),
     ParentTeam: require('./structures/team/ParentTeam'),
-    SubTeam: require('./structures/team/SubTeam')
+    SubTeam: require('./structures/team/SubTeam'),
+    PointsHandler: require('./structures/handlers/TeamsHandler'),
+    SubPointsHandler: require('./structures/handlers/SubPointsHandler'),
+    TeamsHandler: require('./structures/handlers/TeamsHandler')
 };
