8db67f5aef8007341ed5b782a4afe0ab550e40b1
HarmoGlace <harmoglacehelp@gmail.com>
2020-05-24 12:47:09 +0200

    Added jsdoc

--- src/structures/handlers/TeamsHandler.js
+++ src/structures/handlers/TeamsHandler.js
@@ -18,8 +18,19 @@ class TeamsHandler {
     constructor(manager) {
         this.manager = manager;
 
+        /**
+         * All teams of this TeamsManager
+         * @returns {Team[]|Array<ParentTeam|SubTeam>}
+         */
+
         this.all = [];
     }
+
+    /**
+     * Add a Team to this TeamsManager
+     * @param {TeamResolvable} team Team to add
+     * @returns {SubTeam|ParentTeam|Team}
+     */
 
     add(team) {
 
@@ -92,6 +103,12 @@ class TeamsHandler {
 
     }
 
+    /**
+     * Remove a teams from this TeamsManager
+     * @param {TeamResolvable} team Team to remove
+     * @returns {Team[]|Array<ParentTeam|SubTeam>}
+     */
+
     remove(teamRaw) {
 
         if (!teamRaw || !(teamRaw instanceof Team)) throw new TeamyError(`You need to provide a valid Team to delete`);
@@ -104,6 +121,12 @@ class TeamsHandler {
         return this.all;
 
     }
+
+    /**
+     * Remove all teams to keep have given teams
+     * @param {Team|ParentTeam|SubTeam} teams Teams to keep
+     * @returns {Team[]|Array<ParentTeam|SubTeam>}
+     */
 
     set(teams) {
         if (!(teams instanceof Array)) throw new TeamyError(`You must specify an array in <TeamsManager>.teams.set, instead received ${teams.constructor.name})`);
@@ -119,16 +142,28 @@ class TeamsHandler {
 
     }
 
-
+    /**
+     * All ParentTeam of this TeamsHandler
+     * @returns {ParentTeam[]}
+     */
 
     parents() {
         return this.all.filter(team => team.type && team.type === 'parent');
     }
 
+    /**
+     * All SubTeam of this TeamsHandler
+     * @returns {SubTeam[]}
+     */
+
     subs() {
         return this.all.filter(team => team.type && team.type === 'sub');
     }
 
+    /**
+     * Teams sorted by their points
+     * @returns {Team[]|ParentTeam[]}
+     */
 
     get sorted() {
         if (this.manager.type === 'basic') return this.all.sort((a, b) => b.points.get() - a.points.get());
@@ -142,13 +177,31 @@ class TeamsHandler {
         return parents.sort((a, b) => b.points.get() - a.points.get());
     }
 
+    /**
+     * Find a team with a function
+     * @param {String} findFunction function passed to find a team
+     * @returns {Team|ParentTeam|SubTeam|null}
+     */
+
     find(findFunction) {
         return this.all.find(findFunction) || null;
     }
 
+    /**
+     * Find a team with its id
+     * @param {String} id Id of the team to find
+     * @returns {Team|ParentTeam|SubTeam|null}
+     */
+
     get(id) {
         return this.find(team => team.id === id);
     }
+
+    /**
+     * Resolve a team with a string
+     * @param {String} resolvable
+     * @returns {Team|ParentTeam|SubTeam|null}
+     */
 
     resolve(resolvable) {
         resolvable = resolvable.toLowerCase();
