6d41b9d752ec19d24a02003b1003ee8bf25e0b11
HarmoGlace <harmoglacehelp@gmail.com>
2020-09-13 15:03:16 +0200

    Documented GuildMemberHandler

--- src/structures/handlers/GuildMemberHandler.js
+++ src/structures/handlers/GuildMemberHandler.js
@@ -3,6 +3,10 @@ const PointsHandler = require('./PointsHandler');
 
 module.exports = (GuildMember, manager) => {
 
+    /**
+     * GuildMemberHandler
+     */
+
     return class GuildMemberHandler extends GuildMember {
 
         #internalPoints
@@ -20,17 +24,37 @@ module.exports = (GuildMember, manager) => {
             this.#internalPoints = new SubPointsHandler(this);
         }
 
+        /**
+         * Get the current member team
+         * @return {Team|SubTeam|null|AnyTeam}
+         */
+
         get team () {
             return this.manager.getMemberTeam(this);
         }
+
+        /**
+         * Get the current GuildMember teams
+         * @return {Team[]|SubTeam[]|AnyTeam[]}
+         */
 
         get teams () {
             return this.manager.getMemberTeams(this);
         }
 
+        /**
+         * Returns the PointsHandler of this GuildMember
+         * @return {*|PointsHandler}
+         */
+
         get points () {
             return this.team ? this.#internalPoints : new PointsHandler(this);
         }
+
+        /**
+         * Get the parent of this GuildMember (The team of this GuildMember)
+         * @return {Team|SubTeam|null|AnyTeam}
+         */
 
         get parent () {
             return this.team;
